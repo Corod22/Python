906ded7599778b5737572e6c7448eecd287fb921
Alexander <continenttal2015@yandex.ru>
2023-05-31 15:25:05 +0300

    Completed task 14

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
-print("Привет")
+N = abs(int(input('Введите число N ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
